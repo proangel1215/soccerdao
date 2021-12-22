ee1f0b3e8c4af5463ddd3c7ae505200e7fb11e19
proangel1215 <proangel1215@gmail.com>
2021-12-22 12:57:54 -0500

    created a collection

---
+++ scripts/3-config-nft.js
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule('0x5ebB9FdCCED59a2722a390D370fe4b8a0bAA7849');
+
+(async () => {
+    try {
+        await bundleDrop.createBatch({
+            name: "A boy hoping to be a star",
+            description: "This NFT will give you access to SoccerDAO!",
+            image: readFileSync("scripts/assets/pass-nft.png"),
+        });
+        console.log("✅ Successfully created a new NFT in the drop!");
+    } catch (error) {
+        console.error("failed to create the new NFT", error);
+    }
+})();
