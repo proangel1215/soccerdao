b4595e6de0367c0b788bb156a5ccdce87c9ce172
proangel1215 <proangel1215@gmail.com>
2021-12-27 07:48:01 -0500

    added voting functionality for the users

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ import { ethers } from "ethers";
 // We instatiate the sdk on Rinkeby.
 const sdk = new ThirdwebSDK("rinkeby");
 
-// We can grab a reference to our ERC-1155 contract.
 const bundleDropModule = sdk.getBundleDropModule('0x4f87e29bA7Ee65e997adDb20BA84bCC4d64A8d5a');
-// We can grab a reference to our ERC-20 token contract.
 const tokenModule = sdk.getTokenModule('0x6A71E4Ce8E12fAf65D0cF8E1ae935DAc9cF334b0');
+const voteModule = sdk.getVoteModule("0x91165ce03cb75EC9CE650BD89C5Ef4f02cB41D9E");
 
 // A fancy function to shorten someones wallet address, no need to show the whole thing. 
 const shortenAddress = address => address.substring(0, 8) + "..." + address.substring(address.length - 4);
@@ -32,6 +31,10 @@ const App = () => {
   const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
   // The array holding all of our members addresses.
   const [memberAddresses, setMemberAddresses] = useState([]);
+
+  const [proposals, setProposals] = useState([]);
+  const [isVoting, setIsVoting] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
 
   useEffect(() => {
     if ( !hasClaimedNFT ) return;
@@ -57,7 +60,33 @@ const App = () => {
       .catch((err) => {
         console.error("failed to get token amounts: ", err);
       });
+
+    // Retrieve all our existing proposals from the contract.
+    voteModule
+      .getAll()
+      .then(proposals => {
+        setProposals(proposals);
+        console.log("🌈 Proposals:", proposals)
+      })
+      .catch(err => {
+        console.error("failed to get proposals", err);
+      });
   }, [hasClaimedNFT]);
+
+  // We also need to check if the user already voted.
+  useEffect(() => {
+    if ( !hasClaimedNFT || !proposals.length )  return;
+    // Check if the user has already voted on the first proposal.
+    voteModule
+      .hasVoted(proposals[0].proposalId, address)
+      .then(hasVoted => {
+        setHasVoted(hasVoted);
+        if ( hasVoted ) console.log("🥵 User has already voted")
+      })
+      .catch((err) => {
+        console.error("failed to check if wallet has voted", err);
+      });
+  }, [hasClaimedNFT, proposals, address]);
 
   // Now, we combine the memberAddresses and memberTokenAmounts into a single array
   const memberList = useMemo(() => {
@@ -135,6 +164,80 @@ const App = () => {
   // This is the case where we have the user's address
   // which means they've connected their wallet to our site!
   if (hasClaimedNFT) {
+
+    const submitVote = async (evt) => {
+      evt.preventDefault();
+      evt.stopPropagation();
+      
+      setIsVoting(true);
+
+      // lets get the votes from the form for the values
+      const votes = proposals.map((proposal) => ({
+        proposalId: proposal.proposalId,
+        vote: document.querySelector('input[name="' + proposal.proposalId + '"]:checked').value,
+      }));
+
+      // first we need to make sure the user delegates their token to vote
+      try {
+        //we'll check if the wallet still needs to delegate their tokens before they can vote
+        const delegation = await tokenModule.getDelegationOf(address);
+        // if the delegation is the 0x0 address that means they have not delegated their governance tokens yet
+        if ( delegation === ethers.constants.AddressZero ) {
+          //if they haven't delegated their tokens yet, we'll have them delegate them before voting
+          await tokenModule.delegateTo(address);
+        }
+
+        // then we need to vote on the proposals
+        try {
+          await Promise.all(
+            votes.map(async vote => {
+              // before voting we first need to check whether the proposal is open for voting
+              // we first need to get the latest state of the proposal
+              const proposal = await voteModule.get(vote.proposalId);
+              // then we check if the proposal is open for voting (state === 1 means it is open)
+              if ( proposal.state === 1 ) {
+                // if it is open for voting, we'll vote on it
+                return voteModule.vote(vote.proposalId, vote.vote);
+              }
+              // if the proposal is not open for voting we just return nothing, letting us continue
+              return;
+            })
+          );
+
+          try {
+            // if any of the propsals are ready to be executed we'll need to execute them
+            // a proposal is ready to be executed if it is in state 4
+            await Promise.all(
+              votes.map(async vote => {
+                // we'll first get the latest state of the proposal again, since we may have just voted before
+                const proposal = await voteModule.get(vote.proposalId);
+                //if the state is in state 4 (meaning that it is ready to be executed), we'll execute the proposal
+                if ( proposal.state === 4 ) {
+                  // if it is open for voting, we'll vote on it
+                  return voteModule.execute(vote.proposalId);
+                }
+              })
+            );
+
+            // if we get here that means we successfully voted, so let's set the "hasVoted" state to true
+            setHasVoted(true);
+            // and log out a success message
+            console.log("successfully voted");
+
+          } catch (err) {
+            console.error("failed to execute votes", err);
+          }
+        } catch (err) {
+          console.error("failed to vote", err);
+        }
+      } catch (err) {
+        console.error("failed to delegate tokens");
+      } finally {
+        // in *either* case we need to set the isVoting state to false to enable the button again
+        setIsVoting(false);
+      }
+    }
+
     return (
       <div className="member-page">
         <h1>⚽ SoccerDAO Member Page ⚽</h1>
@@ -154,7 +257,7 @@ const App = () => {
                 {
                   memberList.map((member) => (
                     <tr key={member.address}>
-                      <td>{(member.address==address) ? "👉" : "⚽" }</td>
+                      <td>{(member.address===address) ? "👉" : "⚽" }</td>
                       <td>{shortenAddress(member.address)}</td>
                       <td>{member.tokenAmount}</td>
                     </tr>
@@ -162,6 +265,42 @@ const App = () => {
                 }
               </tbody>
             </table>
+          </div>
+          <div>
+            <h2>Active proposals</h2>
+            <form onSubmit={(evt) => submitVote(evt)}>
+              {
+                proposals.map((proposal) => (
+                  <div key={proposal.proposalId} className="card">
+                    <h4>{proposal.description}</h4>
+                    <div>
+                      {proposal.votes.map(vote => (
+                        <div key={vote.type}>
+                          <input
+                            type="radio"
+                            value={vote.type}
+                            id={proposal.proposalId + "-" + vote.type}
+                            name={proposal.proposalId}
+                            defaultChecked={vote.type === 2}
+                          />
+                          <label
+                            htmlFor={proposal.proposalId + "-" + vote.type}
+                          >
+                            {vote.label}
+                          </label>
+                        </div>
+                        )
+                      )}
+                    </div>
+                  </div>
+                ))
+              }
+              <button disabled={isVoting || hasVoted} type="submit">
+                {
+                  isVoting ? "Voting..." : (hasVoted ? "Already Voted!!!" : "Submit Vote!")
+                }
+              </button>
+            </form>
           </div>
         </div>
       </div>
